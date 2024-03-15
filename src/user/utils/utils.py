630dc5a86cf2df9619068fcba103f27062f4d363
Aaryan-0s <aaryanbasnet16@gmail.com>
2024-03-15 08:45:54 +0545

    deleted

--- src/user/utils/utils.py
+++
@@ -1,9 +0,0 @@
-from passlib.context import CryptContext
-
-pwd_crypt=CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-def hash(password: str):
-    return pwd_crypt.hash(password)
-
-def verify_password(plain_password, hashed_password):
-    return pwd_crypt.verify(plain_password, hashed_password)
